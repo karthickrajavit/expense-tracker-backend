3c735beea4b385060b5e9ee12d2cdeb2008d12aa
Karthick raja V <karthickrajavit@gmail.com>
2025-03-29 13:53:46 +0530

    Updated firebase admin to get the credentials from local machine

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,10 +1,9 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./expense-tracker-9295e-firebase-adminsdk-fbsvc-956bf0e465.json"); // Update with your service account file
 
 // Check if Firebase Admin is already initialized
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.applicationDefault(),
   });
 }
 
